c7a66af8a076285bc0d518fb830e8912f60c9d18
ThlonDev <yuthlon157@gmail.com>
2025-07-25 22:29:39 +0900

    実装完了

--- main.ps1
+++ main.ps1
@@ -1,7 +1,8 @@
-Param(
-    [String]$SimuInPath = "C:\Program Files (x86)\Simutrans\",
-    [String]$ExeFileName = "simutrans-OTRP.exe"
-)
+$configFilePath = "config.json"
+$config = Get-Content -Path $configFilePath -Raw | ConvertFrom-Json
+
+$SimuInPath = $config.Config.INSTALL_PATH
+$ExeFileName = $config.Config.EXEFILE_NAME
 
 $API_DATA = curl https://api.github.com/repos/teamhimeh/simutrans/releases/latest
 $JSON = $API_DATA | ConvertFrom-Json
@@ -10,8 +11,15 @@ $DOWNLOAD_URL = $JSON.assets.browser_download_url[2]
 Write-Host $VERSION
 Write-Host $DOWNLOAD_URL
 (New-Object System.Net.WebClient).DownloadFile($DOWNLOAD_URL,"OTRP.zip")
-Expand-Archive -Path .\OTRP.zip -DestinationPath .\OTRP\
-$EXE = Get-ChildItem -Filter *.exe -Path .\OTRP\
-Rename-Item -Path $EXE -NewName $ExeFileName
-Copy-Item -Filter *.exe -Path .\OTRP\* -Destination $SimuInPath
-Remove-Item -Path 
+Expand-Archive -Path OTRP.zip -DestinationPath .\temp\ -Force
+$EXE = Get-ChildItem -Filter *.exe -Path temp
+$ALLFILEPATH = $SimuInPath + $ExeFileName
+$TEMPEXEPATH = ".\temp\" + $EXE
+Write-Host $EXE
+Write-Host $SimuInPath
+Write-Host $ExeFileName
+Write-Host $ALLFILEPATH
+Rename-Item -Path $TEMPEXEPATH -NewName $ExeFileName
+Copy-Item -Filter *.exe -Path .\temp\* -Destination $SimuInPath -Force
+Remove-Item -Path .\OTRP.zip
+Remove-Item -Recurse -Path .\temp\
